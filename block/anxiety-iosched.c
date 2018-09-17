f6a62609d1b4bc885c463488c27128133ad9b155
kdrag0n <dragon@khronodragon.com>
2018-09-16 18:34:14 -0700

    block: anxiety-iosched: don't use size_t type for starvation
    size_t should not be used in this case.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Michael <loukerismichalis@gmail.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -14,8 +14,8 @@
 enum {ASYNC, SYNC};
 
 struct anxiety_data {
-	struct list_head queue[2][2];
-	size_t writes_starved;
+	struct list_head queue[2];
+	unsigned int writes_starved;
 };
 
 static void anxiety_merged_requests(struct request_queue *q, struct request *rq, struct request *next)
