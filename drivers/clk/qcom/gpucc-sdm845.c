6c07db58cea428bec0478673940237cf8b09e27f
dakkshesh07 <dakkshesh5@gmail.com>
2020-11-29 21:13:23 +0530

    reduced gpu overclock to 827mhz

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -285,7 +285,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
 	F(767000000, P_CRC_DIV,  1, 0, 0),
-	F(833000000, P_CRC_DIV,  1, 0, 0),
+	F(827000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -648,7 +648,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 710000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_1] = 767000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		833000000;
+		827000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
